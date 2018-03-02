c1011b3764c4b7269fb6b3a29a295b7517d3e594
ReaperLC <13443207@qq.com>
2018-03-02 14:47:44 +0800

    app各阶段运行耗时

---
+++ speedOfLaunch/speedOfLaunch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  speedOfLaunch
+//
+//  Created by 刘灿 on 2018/3/2.
+//  Copyright © 2018年 ReaperLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ speedOfLaunch/speedOfLaunchTests/speedOfLaunchTests.m
@@ -0,0 +1,39 @@
+//
+//  speedOfLaunchTests.m
+//  speedOfLaunchTests
+//
+//  Created by 刘灿 on 2018/3/2.
+//  Copyright © 2018年 ReaperLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface speedOfLaunchTests : XCTestCase
+
+@end
+
+@implementation speedOfLaunchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
